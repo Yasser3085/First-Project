1606e07c1a5fca7cbd3efb6e8d9b48c0ba3d20bd
Yasser3085 <yasser30855@gmail.com>
2023-05-01 13:49:02 +0300

    adding ejs

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ const express = require("express");
 // step 2
 const app = express();
 
+// telling node Js to use (ejs) as the view engine
+
+app.set("view engine", "ejs");
+
 //step 4
 
 app.get("/name", (req, res) => {
   res.send(" Last Change ");
+});
+
+app.get("/testhtml", (req, res) => {
+  res.sendFile(__dirname + "/test.html");
+});
+
+app.get("/testEjs", (req, res) => {
+  const student = {
+    grade: 100
+  };
+  res.render( "introToEjs.ejs",student);
 });
 
 // step 3
